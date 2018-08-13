5daebda48354ba953883f36b92b8db7ad590d934
Lorenzo Ibay <31312773+renzo324@users.noreply.github.com>
2018-08-13 17:25:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-const API_KEY= AIzaSyD36fANxC_S5fqSsgJpIFf0BcsFmGrPXrs;
-
-
-function getData() {
-    var endPoint = "functions.php?function=getUsers";
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', endPoint, true);
-    xhr.onload = function() {
-        if (this.status == 200) {
-            var r = JSON.parse(this.response);
-            var x = '';
-
-            for (var i = 0, len = r.length; i < len; i++) {
-            x +=  ;
-            }
-
-            document.getElementById('app').innerHTML = x;
-
-
-        };
-    };
-    xhr.send();
+window.document.onload = welcome();
+document.getElementById("home").addEventListener("click", home);
+function home(){
+    location.reload('app');
+}
+function welcome(){
+    var x= '<div class= "row justify-content-center"> <div class="col">';
+    x += '<div class="jumbotron jumbotron-fluid">';
+    x += '<div class="container">';
+    x += '<h1 class="display-4">Welcome to HomeBrew!</h1>';
+    x += '<p class="lead">A place to share and discover new ways to enjoy coffee.</p>';
+    x += '<ul><li><a id="find" class="btn btn-secondary" href="#"> Find Coffee! </a></li>';
+    x += '<li><a id="partner" class="btn btn-secondary" href="#"> Be a Partner! </a></li>';
+    x += '<li><a id="signUp" class="btn btn-secondary" href="#"> Sign Up! </a></li></ul>';
+    x += ' </div> </div> </div> </div>';
+    document.getElementById('app').innerHTML = x;
 }
 
-
-
+document.getElementById("find").addEventListener("click", find);
+function find(){
+    var x= '<h1>Partner Listings go here</h1>';
+    x += '';
+    document.getElementById('app').innerHTML = x;
+}
+document.getElementById("partner").addEventListener("click", partner);
+function partner(){
+    var x ='<h1> Parter signup goes here</h1>'
+    x+= ''
+    document.getElementById('app').innerHTML = x;
+}
+document.getElementById("signUp").addEventListener("click", signUp);
+function signUp(){
+    var x ='<h1> Customer signup goes here</h1>';
+    x+= '';
+    document.getElementById('app').innerHTML = x;
+}
+  
+    
+   
+ 
