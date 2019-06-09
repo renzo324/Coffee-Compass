131b1a6d1a70e79f46d7dddfc3103c151af2d66c
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-06-10 02:52:59 +0800

    add sql

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "caffeine";
+$dbname     = "lorenzot_caffeine";
 $conn       = new mysqli($servername, $username, $password, $dbname);
 
 
